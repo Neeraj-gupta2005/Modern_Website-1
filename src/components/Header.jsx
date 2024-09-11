938e0f848a693cf45c8acdc9ce416a67830495fe
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-11 22:16:09 +0530

    made header scroll lock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import Button from '../components/Button';
 import {HamburgerMenu} from "./design/Header";
 import MenuSvg from './../assets/svg/MenuSvg';
 import { useState } from 'react';
+import { disablePageScroll ,  enablePageScroll} from 'scroll-lock';
 
 const Header = () => {
 
@@ -13,18 +14,20 @@ const Header = () => {
   const toggleNavigation = () => {
     if(openNavigation) {
       setOpenNavigation(false)
+      enablePageScroll()
     } else {
       setOpenNavigation(true)
+      disablePageScroll()
     }
   };
 
   const handleClick = (e) => {
     setOpenNavigation(false);
+    enablePageScroll();
   };
 
   return (
-    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90
-    backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 
+    <div className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 
     lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : `bg-n-8/90 backdrop-blur-sm `}`}>
         <div className="flex justify-between items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ">
           <a className='block w-[12rem] xl:mr-8' href='#hero'>
@@ -35,7 +38,7 @@ const Header = () => {
             <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
 
               {navigation.map((item) => (
-                <a key={item.id} href={item.url}
+                <a onClick={handleClick} key={item.id} href={item.url}
                 className={
                   `block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? `lg:hidden` : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === location.hash ? `z-2 lg:text-n-1` : `lg:text-n-1/50`} lg:leading-5 xl:px-12`}
                 >
