12674764bc99d3084954bf546b008fe7b471731d
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-15 11:37:50 +0530

    made collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Button from './components/Button';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
